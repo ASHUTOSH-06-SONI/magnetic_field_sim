3cd01e05a2abbc32b39b3916de5cf03ee5f49b45
Ashutosh Soni <ashusoni1892@gmail.com>
2026-09-25 19:02:56 +0530

    making it collide against the boundary

--- magnetism/anotherone.cpp
+++ magnetism/anotherone.cpp
@@ -4,10 +4,31 @@ int main(){
     sf::RenderWindow window(sf::VideoMode({1280,720}),"from scratch");
     window.setFramerateLimit(60);
     sf::CircleShape cir;
-    cir.setPosition(sf::Vector2f(400.f,300.f));
+    sf::Vector2f position(400.f, 300.f);
+    cir.setPosition(position);
     cir.setRadius(5);
-
+    position.x = 1270.f; 
+    position.y = 710.f;
+    sf::Vector2f vel(200.f,200.f);
+    sf::Clock clock;
     while(window.isOpen()){
+        sf::Time elapsed = clock.restart();
+        float dt = elapsed.asSeconds();
+        position += vel * dt;
+        if(position.x<0){
+            position.x = 0; 
+            vel.x*=-1;
+        }if(position.x>1270){
+            position.x = 1270; 
+            vel.x*=-1;
+        }if(position.y<0){
+            position.y = 0; 
+            vel.y*=-1;
+        }if(position.y>710){
+            position.y = 710; 
+            vel.y*=-1;
+        }
+        cir.setPosition(position);
         while(auto event = window.pollEvent()){
             if(event->is<sf::Event::Closed>()){
                 window.close();
