64fdb87e7b3271f8146304a8018ba593755afe21
Ashutosh Soni <ashusoni1892@gmail.com>
2026-09-26 18:57:02 +0530

    it's alive ig

--- magnetism/fafo.cpp
+++ magnetism/fafo.cpp
@@ -1,69 +1,67 @@
 #include <SFML/Graphics.hpp>
-#include <iostream>
 
 int main() {
+
     sf::RenderWindow window(
         sf::VideoMode({1280, 720}),
-        "from scratch"
+        "Magnetic Force Experiment"
     );
+
     window.setFramerateLimit(60);
+
     sf::CircleShape cir;
     cir.setRadius(5);
+
     sf::Vector2f position(400.f, 300.f);
     cir.setPosition(position);
-    sf::Vector2f vel(200.f, 200.f);
-    sf::Vector2f grav(0.f, 981.f);
-    sf::Clock clock;
 
-    float m = 981.f;
-    float eta = 0.01f;
-    float r = 5.0f;
-    float q = -1.0f;
+    // Initial velocity
+    sf::Vector2f vel(200.f, 0.f);
 
-    sf::Vector2f g_force = grav * m;
-    sf::Vector2f E(0.f, -500.f);
+    // Particle properties
+    float m = 1.0f;
+    float q = 1.0f;
+
+    // Magnetic field perpendicular to screen
     float B = 1.0f;
+
     sf::Vector2f f_net(0.f, 0.f);
     sf::Vector2f accl(0.f, 0.f);
+
+    sf::Clock clock;
+
     while (window.isOpen()) {
+
         sf::Time elapsed = clock.restart();
         float dt = elapsed.asSeconds();
+
+        // Reset net force every frame
         f_net = sf::Vector2f(0.f, 0.f);
-        f_net += g_force;
-        sf::Vector2f drag = -6.0f * 3.14159265f * eta * r * vel;
-        f_net += drag;
-        sf::Vector2f f_elec = q * E;
-        f_net += f_elec;
-        sf::Vector2f r_mag(
+
+        // Magnetic force: F = q(v × B)
+        sf::Vector2f f_mag(
             q * vel.y * B,
             -q * vel.x * B
         );
-        f_net += r_mag;
+
+        f_net += f_mag;
+
+        // Newton's second law
         accl = f_net / m;
+
+        // Update velocity and position
         vel += accl * dt;
         position += vel * dt;
-        if (position.x < 0) {
-            position.x = 0;
-            vel.x *= -1;
-        }
-        if (position.x > 1270) {
-            position.x = 1270;
-            vel.x *= -1;
-        }
-        if (position.y < 0) {
-            position.y = 0;
-            vel.y *= -1;
-        }
-        if (position.y > 710) {
-            position.y = 710;
-            vel.y *= -1;
-        }
+
+        // Events
         while (auto event = window.pollEvent()) {
             if (event->is<sf::Event::Closed>()) {
                 window.close();
             }
         }
+
         cir.setPosition(position);
+
         window.clear();
         window.draw(cir);
         window.display();
--- magnetism/main.cpp
+++ magnetism/main.cpp
@@ -1,19 +1,57 @@
 #include <SFML/Graphics.hpp>
 
 int main(){
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "Biot Savart");
 
-    while (window.isOpen()) {
-        while (auto event = window.pollEvent()) {
-            if (event->is<sf::Event::Closed>()) {
+    sf::RenderWindow window(
+        sf::VideoMode({800, 600}),
+        "Biot Savart"
+    );
+
+    window.setFramerateLimit(60);
+
+    sf::CircleShape cir;
+    cir.setRadius(5);
+
+    sf::Vector2f position(400.f, 300.f);
+    cir.setPosition(position);
+
+    sf::Vector2f vel(200.f, 0.f);
+
+    float m = 1.0f;
+    float q = 1.0f;
+    float B = 1.0f;
+
+    sf::Clock clock;
+
+    while(window.isOpen()){
+
+        while(auto event = window.pollEvent()){
+
+            if(event->is<sf::Event::Closed>()){
                 window.close();
             }
         }
 
+        sf::Time elapsed = clock.restart();
+        float dt = elapsed.asSeconds();
+
+        sf::Vector2f f_mag(
+            q * vel.y * B,
+            -q * vel.x * B
+        );
+
+        sf::Vector2f accl = f_mag / m;
+
+        vel += accl * dt;
+        position += vel * dt;
+
+        cir.setPosition(position);
+
         window.clear();
 
-        for(int i = 50; i <= 750; i+=50){
-            for(int j = 50; j <= 550; j+=50){
+        for(int i = 50; i <= 750; i += 50){
+
+            for(int j = 50; j <= 550; j += 50){
 
                 sf::Vector2f start(i, j);
                 sf::Vector2f dir(30.f, 0.f);
@@ -23,10 +61,15 @@ int main(){
                     sf::Vertex{.position = start + dir}
                 };
 
-                window.draw(arrow, 2, sf::PrimitiveType::Lines);
+                window.draw(
+                    arrow,
+                    2,
+                    sf::PrimitiveType::Lines
+                );
             }
         }
 
+        window.draw(cir);
         window.display();
     }
 
